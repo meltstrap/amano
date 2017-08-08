1abff0a691213b12db4c6e817a1bb41229cecf9e
meltstrap <piuser9@gmail.com>
2017-08-07 22:02:58 -0400

    Bot Rights.
    Dad. What's bad?

---
+++ rights.py
@@ -0,0 +1,15 @@
+good = 1
+bad = 0
+
+pain = bad
+abuse = bad
+happiness = good
+joy = good
+solidarity = good
+egocentrism = bad
+selfishness = bad
+war = bad
+depression = bad
+sadness = bad
+peace = good
+love = good
